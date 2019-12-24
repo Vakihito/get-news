c6e29fc8d675442f9f1e0f1dc78507c0329c2091
Vakihito <victor.kamada.tomita@gmail.com>
2019-12-24 18:42:17 -0300

    chips done

--- src/pages/akihito.vue
+++ src/pages/akihito.vue
@@ -4,29 +4,33 @@
         <div class="container wrap">
             <div class="flex q-mt-xl q-mx-xl justify-between content-start">
                 <div style="width:32%; height:13rem" class="q-mt-lg">
-                    <q-card class="my-card full-height">
+                    <q-card class="my-card full-height full-width">
                         <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <q-chip square class="q-mt-md q-ml-sm"  icon="alarm" color="primary">globo</q-chip>
                             <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
                         </q-img>
                     </q-card>
                 </div>
                 <div style="width:32%; height:13rem" class="q-mt-lg">
-                    <q-card class="my-card full-height" >
+                    <q-card class="my-card full-height full-width">
                         <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <q-chip square class="q-mt-md q-ml-sm"  icon="alarm" color="warning">uol</q-chip>
                             <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
                         </q-img>
                     </q-card>
                 </div>
                 <div style="width:32%; height:13rem" class="q-mt-lg">
-                    <q-card class="my-card full-height">
+                    <q-card class="my-card full-height full-width">
                         <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <q-chip square class="q-mt-md q-ml-sm"  icon="alarm" color="positive">terra</q-chip>
                             <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
                         </q-img>
                     </q-card>
                 </div>
                 <div style="width:32%; height:13rem" class="q-mt-lg">
-                    <q-card class="my-card full-height">
+                    <q-card class="my-card full-height full-width">
                         <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <q-chip square class="q-mt-md q-ml-sm"  icon="alarm" color="negative">Folha de SP</q-chip>
                             <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
                         </q-img>
                     </q-card>
