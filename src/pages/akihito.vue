d3d0b430dd826464bb298a4af51282bafe4cd3a7
Vakihito <victor.kamada.tomita@gmail.com>
2019-12-24 18:22:54 -0300

    init

---
+++ src/pages/akihito.vue
@@ -0,0 +1,42 @@
+<template>
+    <q-page>
+        <!-- all the other news  -->
+        <div class="container wrap">
+            <div class="flex q-mt-xl q-mx-xl justify-between content-start">
+                <div style="width:32%; height:13rem" class="q-mt-lg">
+                    <q-card class="my-card full-height">
+                        <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
+                        </q-img>
+                    </q-card>
+                </div>
+                <div style="width:32%; height:13rem" class="q-mt-lg">
+                    <q-card class="my-card full-height" >
+                        <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
+                        </q-img>
+                    </q-card>
+                </div>
+                <div style="width:32%; height:13rem" class="q-mt-lg">
+                    <q-card class="my-card full-height">
+                        <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
+                        </q-img>
+                    </q-card>
+                </div>
+                <div style="width:32%; height:13rem" class="q-mt-lg">
+                    <q-card class="my-card full-height">
+                        <q-img class="full-height" src="https://cdn.quasar.dev/img/parallax2.jpg">
+                            <div class="text-subtitle3 absolute-bottom text-center">clean description</div>
+                        </q-img>
+                    </q-card>
+                </div>
+            </div>
+        </div>
+    </q-page>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+</style>
